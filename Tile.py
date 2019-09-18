99da3f980d98f308367e0d93ddaaa49c6764a6d4
kapl0001 <kapl0001@stud.hs-kl.de>
2019-09-18 10:38:26 +0200

    Tile Datenmodell erweitert

--- Tile.py
+++ Tile.py
@@ -1,14 +1,15 @@
 
 ## In a tile all informration of the map is stored for a geohash rectangle (e.g. ezs42)
 
-
-
 class Tile:
    ## maps nodeId --> Node object
 
-   def __init__(self):
+   def __init__(self, geoHash):
       """"""
+
       self._nodes = {}
       self._links = {}
-      self._geohash = None
+      self._geohash = geoHash
 
+   def addNode(self, node):
+      self._nodes.update({node._id: node})
