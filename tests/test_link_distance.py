b0f247e50b480500bc1f6b919e9f6a85792a81c2
Sebastian <sele0004@stud.hs-kl.de>
2019-10-18 17:56:50 +0200

    dummy eingefügt

---
+++ tests/test_link_distance.py
@@ -0,0 +1,11 @@
+import unittest
+
+from src.models.link_distance import LinkDistance
+from src.models.link import Link
+from src.models.node import Node
+
+
+class TestLinkDistance(unittest.TestCase):
+
+    def test_link_distance(self):
+        pass
