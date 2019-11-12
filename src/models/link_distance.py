b06887e5390aa9f9d4a3c67c63207bab496dd64e
LukasFelzmann <lufe0002@stud.hs-kl.de>
2019-11-12 12:37:03 +0100

    1) Habe die Methode _calc_matched_point_of_link_segment jetzt doch so implementiert wie in graphHopper. 2) Habe für diverse Methoden die docStrings überarbeitet/hinzugefügt. 3) Kleine Optimierung hinsichtlich der Methode _calc_fraction. Die Segmente des Links werden gleich mitübergeben, so müssen sie nur noch einmal in _initialize_matched_point_and_fraction gebildet werden." 4) Todos hinzugefügt.

--- src/models/link_distance.py
+++ src/models/link_distance.py
@@ -38,6 +38,8 @@ class LinkDistance:
         # Gibt an, ob self schon vollständig initialisiert wurde (für weitere Details siehe _lazy_load(self))
         self.init = False
 
+    # Todo: Besprechen, ob lazy_load wirklich Sinn macht. Wann werden wir schon ein LinkDistance-Objekt erzeugen und
+    #  uns nicht für self.fraction, self._matched_point oder self.distance interessieren?
     def _lazy_load(self):
         """
         Diese Methode schließt die Initialisierung ab.
@@ -58,11 +60,16 @@ class LinkDistance:
 
         return self.fraction
 
-    def _calc_fraction(self, involved_segment):
-        """Take note, that this function, should only be invoked if self._matched_point is already initialized."""
+    def _calc_fraction(self, link_segments, involved_segment):
+        """
+        Take note, that this function, should only be invoked if self._matched_point is already initialized.
+        :param link_segments: the segments of the link
+        :param involved_segment: the link segment, which contains self._matched_point.
+        :return: None
+        """
         distance = 0
 
-        for seg in self._build_link_segments():
+        for seg in link_segments:
             if seg == involved_segment:
                 distance += great_circle(seg[0], self._matched_point)
                 self.fraction = distance/self.link.get_length()
@@ -71,41 +78,84 @@ class LinkDistance:
                 distance += great_circle(seg[0], seg[1])
 
     @staticmethod
-    def _calc_shrink_factor(a_lat, b_lat):
+    def _calc_shrink_factor(a_lat_deg, b_lat_deg):
         """Taken from:
         https://github.com/graphhopper/graphhopper/blob/master/api/src/main/java/com/graphhopper/util
         /DistanceCalcEarth.java """
-        return math.cos(math.radians((a_lat + b_lat) / 2))
+        return math.cos(math.radians((a_lat_deg + b_lat_deg) / 2))
 
-    @staticmethod
-    def _orthogonal_projection(vector_from, vector_to):
-        return (numpy.vdot(vector_from, vector_to) / numpy.vdot(vector_to, vector_to)) * vector_to
+    # @staticmethod
+    # def _orthogonal_projection(vector_from, vector_to):
+    #     return (numpy.vdot(vector_from, vector_to) / numpy.vdot(vector_to, vector_to)) * vector_to
 
     def _calc_matched_point_of_link_segment(self, a: tuple, b: tuple) -> tuple:
-        """Calculates the matched point of a link_segment like the method calcCrossingPointToEdge in:
+        """
+        Calculates the matched point of a link_segment like the method calcCrossingPointToEdge in:
         https://github.com/graphhopper/graphhopper/blob/master/api/src/main/java/com/graphhopper/util
-        /DistanceCalcEarth.java """
+        /DistanceCalcEarth.java
+        Take note, that here is essentially performed a orthogonal projection.
 
-        shrink_factor = LinkDistance._calc_shrink_factor(a[0], b[0])
-        segment_vector = numpy.array([b[0], b[1] * shrink_factor]) - numpy.array([a[0], a[1] * shrink_factor])
-        point_vector = numpy.array([self._lat_lon[0], self._lat_lon[1] * shrink_factor]) - numpy.array(a)
+        :param a: beginning of the link-segment
+        :param b: end of the link-segment
+        :return: matched point of the link-segment
+        """
 
-        orthogonal_projection = LinkDistance._orthogonal_projection(point_vector, segment_vector)
+        a_lat_deg, a_lon_deg = a
+        b_lat_deg, b_lon_deg = b
+        r_lat_deg, r_lon_deg = self._lat_lon
 
-        if orthogonal_projection[0] * segment_vector[0] < 0:
-            matched_point_vector = numpy.array(a)
+        shrink_factor = self._calc_shrink_factor(a_lat_deg, b_lat_deg)
 
+        a_lat = a_lat_deg
+        a_lon = a_lon_deg * shrink_factor
+
+        b_lat = b_lat_deg
+        b_lon = b_lon_deg * shrink_factor
+
+        r_lat = r_lat_deg
+        r_lon = r_lon_deg * shrink_factor
+
+        delta_lon = b_lon - a_lon
+        delta_lat = b_lat - a_lat
+
+        matched_point = None
+
+
+        # Bin mir noch nicht sicher, ob diese Fallunterscheidung wirklich oft zum Tragen kommt.
+        # Todo: Klären, ob diese Fallunterscheidung rein soll oder nicht.
+        # if delta_lat == 0:
+        #     # special case: horizontal edge
+        #     #return new GHPoint(a_lat_deg, r_lon_deg);
+        #     factor =
+        #
+        # if delta_lon == 0:
+        #     # special case: vertical edge
+        #     # return new GHPoint(r_lat_deg, a_lon_deg);
+        #     factor =
+
+        # ------------------------------------
+
+
+        norm = delta_lon * delta_lon + delta_lat * delta_lat
+        factor = ((r_lon - a_lon) * delta_lon + (r_lat - a_lat) * delta_lat) / norm
+
+        if factor >= 1:
+            matched_point = b
+        elif factor <= 0:
+            matched_point = a
         else:
-            if numpy.linalg.norm(orthogonal_projection) > numpy.linalg.norm(segment_vector):
-                matched_point_vector = numpy.array(b)
-            else:
-                matched_point_vector = numpy.array(a) + orthogonal_projection
-
-        matched_point = (matched_point_vector[0], matched_point_vector[1] / shrink_factor)
+            c_lat = a_lat + factor * delta_lat
+            c_lon = a_lon + factor * delta_lon
+            matched_point = (c_lat, c_lon/shrink_factor)
 
         return matched_point
 
     def _initialize_matched_point_and_fraction(self):
+        """
+        Performs the initialization of the attributes _matched_point and _fraction.
+
+        :return: None
+        """
 
         link_segments = self._build_link_segments()
         min_distance = 20037000  # biggest range of earth: 40074000 Meter
@@ -121,10 +171,16 @@ class LinkDistance:
                 involved_segment = seg
 
         self._matched_point = matched_point
-        self._calc_fraction(involved_segment)
+        self._calc_fraction(link_segments, involved_segment)
 
     def _build_link_segments(self) -> list:
+        """
+        Splits a link into segments, each consisting of two positions/coordinates.
+
+        :return: list, containing the segments
+        """
         segments = list()
+
         for i in range(len(self.link.__geometry) - 1):
             segment = (self.link.__geometry[i], self.link.__geometry[i+1])
             segments.append(segment)
