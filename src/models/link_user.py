a323c8b0f5d5304861e39d579c49bc2528409c35
LukasFelzmann <lufe0002@stud.hs-kl.de>
2019-11-17 12:28:08 +0100

    Habe nun in den Klassen Car, Cyclist und Pedestrian auch in der Methoden _may_use() jeweils eine Liste an Regeln angelegt.

--- src/models/link_user.py
+++ src/models/link_user.py
@@ -76,27 +76,42 @@ class Pedestrian(LinkUser):
             return False
 
     def _may_use(self, link) -> bool:
+
         tags = link.get_tags()
         highway_val = tags.get("highway")
         foot_val = tags.get("foot")
-
-        if highway_val is not None:
-            if highway_val in {"residential", "living_street", "bridleway", "path"}:
-                if foot_val != "no":
-                    return True
-
-            elif highway_val in {"pedestrian", "footway", "steps"}:
-                return True
-
         sidewalk_val = tags.get("sidewalk")
-        if sidewalk_val in {"both", "left", "right"}:
-            if foot_val not in {None, "no"}:
-                return True
+        rules = [highway_val is not None and highway_val in {"residential", "living_street", "bridleway", "path"} and
+                 foot_val != "no", highway_val is not None and highway_val in {"pedestrian", "footway", "steps"},
+                 sidewalk_val in {"both", "left", "right"} and foot_val not in {None, "no"}, foot_val in {"yes", "designated", "permissive"}]
 
-        if foot_val in {"yes", "designated", "permissive"}:
+        if True in rules:
             return True
+        else:
+            return False
 
-        return False
+        # So war es vorher:
+        # tags = link.get_tags()
+        # highway_val = tags.get("highway")
+        # foot_val = tags.get("foot")
+        #
+        # if highway_val is not None:
+        #     if highway_val in {"residential", "living_street", "bridleway", "path"}:
+        #         if foot_val != "no":
+        #             return True
+        #
+        #     elif highway_val in {"pedestrian", "footway", "steps"}:
+        #         return True
+        #
+        # sidewalk_val = tags.get("sidewalk")
+        # if sidewalk_val in {"both", "left", "right"}:
+        #     if foot_val not in {None, "no"}:
+        #         return True
+        #
+        # if foot_val in {"yes", "designated", "permissive"}:
+        #     return True
+        #
+        # return False
 
 
 class Cyclist(LinkUser):
@@ -175,36 +190,54 @@ class Cyclist(LinkUser):
         highway_val = tags.get("highway")
         bicycle_val = tags.get("bicycle")
 
-        if bicycle_val == "no":
+        rules = [bicycle_val != "no" and highway_val is not None and (highway_val in {"residential", "cycleway", "bridleway", "path"} or (highway_val == "steps" and tags.get("ramp:bicycle") == "yes")),
+                 bicycle_val not in {"no", None} and bicycle_val in {"yes", "designated", "use_sidepath", "permissive", "destination"},
+                 tags.get("cycleway") not in {None, "no"}, tags.get("bicycle_road") == "yes",
+                 tags.get("cyclestreet") == "yes", tags.get("cycleway:right") is not None or tags.get("cycleway:left")
+                 is not None or tags.get("cycleway:both") is not None]
+
+        if True in rules:
+            return True
+        else:
             return False
 
-        if highway_val is not None:
 
-            if highway_val in {"residential", "cycleway", "bridleway", "path"}:
-                return True
-
-            elif highway_val == "steps":
-                if tags.get("ramp:bicycle") == "yes":
-                    return True
-
-        if bicycle_val is not None:
-            if bicycle_val in {"yes", "designated", "use_sidepath", "permissive", "destination"}:
-                return True
-
-        if tags.get("cycleway") not in {None, "no"}:
-            return True
-
-        if tags.get("bicycle_road") == "yes":
-            return True
-
-        if tags.get("cyclestreet") == "yes":
-            return True
-
-        if tags.get("cycleway:right") is not None or tags.get(
-                "cycleway:left") is not None or tags.get("cycleway:both") is not None:
-            return True
-
-        return False
+        # So war es vorher:
+        # tags = link.get_tags()
+        #
+        # highway_val = tags.get("highway")
+        # bicycle_val = tags.get("bicycle")
+        #
+        # if bicycle_val == "no":
+        #     return False
+        #
+        # if highway_val is not None:
+        #
+        #     if highway_val in {"residential", "cycleway", "bridleway", "path"}:
+        #         return True
+        #
+        #     elif highway_val == "steps":
+        #         if tags.get("ramp:bicycle") == "yes":
+        #             return True
+        #
+        # if bicycle_val is not None:
+        #     if bicycle_val in {"yes", "designated", "use_sidepath", "permissive", "destination"}:
+        #         return True
+        #
+        # if tags.get("cycleway") not in {None, "no"}:
+        #     return True
+        #
+        # if tags.get("bicycle_road") == "yes":
+        #     return True
+        #
+        # if tags.get("cyclestreet") == "yes":
+        #     return True
+        #
+        # if tags.get("cycleway:right") is not None or tags.get(
+        #         "cycleway:left") is not None or tags.get("cycleway:both") is not None:
+        #     return True
+        #
+        # return False
 
 
 class Car(LinkUser):
@@ -238,6 +271,9 @@ class Car(LinkUser):
             tags = link.get_tags()
             highway_val = tags.get("highway")
             oneway_val = tags.get("oneway")
+
+            # Todo (13.11.2019, Lukas Felzmann): Nochmal sicherstellen (durch Recherche), ob highway=motorway
+            #  auch wirklich nicht zusammen mit oneway=no oder oneway=-1 verwendet wird/werden kann.
             rules = [highway_val != "motorway" and (highway_val != "trunk" or oneway_val in {"no", "-1"}) and oneway_val != "yes"]
 
             if True in rules:
@@ -267,20 +303,35 @@ class Car(LinkUser):
             return False
 
     def _may_use(self, link) -> bool:
+
         tags = link.get_tags()
         highway_val = tags.get("highway")
         motor_vehicle_val = tags.get("motor_vehicle")
         motorcar_val = tags.get("motorcar")
+        rules = [highway_val is not None and highway_val in {"motorway", "trunk", "primary", "secondary", "tertiary", "motorway_link", "trunk_link", "primary_link", "secondary_link", "tertiary_link"},
+                 highway_val is not None and highway_val in {"unclassified", "residential", "living_street"} and motor_vehicle_val != "no" and motorcar_val != "no",
+                 motor_vehicle_val == "yes" or motorcar_val == "yes"]
 
-        if highway_val is not None:
-            if highway_val in {"motorway", "trunk", "primary", "secondary", "tertiary", "motorway_link", "trunk_link",
-                               "primary_link", "secondary_link", "tertiary_link"}:
-                return True
-            elif highway_val in {"unclassified", "residential",
-                                 "living_street"} and motor_vehicle_val != "no" and motorcar_val != "no":
-                return True
-
-        if motor_vehicle_val == "yes" or motorcar_val == "yes":
+        if True in rules:
             return True
+        else:
+            return False
 
-        return False
+        # So war es vorher:
+        # tags = link.get_tags()
+        # highway_val = tags.get("highway")
+        # motor_vehicle_val = tags.get("motor_vehicle")
+        # motorcar_val = tags.get("motorcar")
+        #
+        # if highway_val is not None:
+        #     if highway_val in {"motorway", "trunk", "primary", "secondary", "tertiary", "motorway_link", "trunk_link",
+        #                        "primary_link", "secondary_link", "tertiary_link"}:
+        #         return True
+        #     elif highway_val in {"unclassified", "residential",
+        #                          "living_street"} and motor_vehicle_val != "no" and motorcar_val != "no":
+        #         return True
+        #
+        # if motor_vehicle_val == "yes" or motorcar_val == "yes":
+        #     return True
+        #
+        # return False
