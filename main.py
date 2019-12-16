5f49f85e448006fb7c0a07ea0efc26e908928293
kapl0001 <kapl0001@stud.hs-kl.de>
2019-12-16 15:20:26 +0100

    Imports raus

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 from src.map_service import MapService
-from src.models.bounding_box import BoundingBox
-from src.models import Link
-from src.models import LinkId
-from src.models import Pedestrian, Cyclist, Car
-from src.models import NodeId
-from src.utils.router import RouterDijkstra
+
 from src.geohash_wrapper import GeoHashWrapper
 from src.rest.app import app
 
 
 def main():
     ghw = GeoHashWrapper()
-    print(ghw._get_neighbors("rzzz"))
+    print(ghw.get_neighbors("rzzz"))
 
 
 def start_server():
